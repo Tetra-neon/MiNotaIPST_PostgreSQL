830b9763e54375ecfedab50441e9d3fe9adece1f
Tiffany Baron <tiffanybaron1998@gmail.com>
2025-04-27 18:03:26 -0400

    Protección de APIs de asignaturas y notas con autenticación JWT. Predicción también protegida

--- notas/urls.py
+++ notas/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AsignaturaViewSet, NotaViewSet
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Asignatura #para saber de qué modelo estamos hablando
-from django.shortcuts import get_object_or_404 #Para buscar la asignatura por id
+from .views import AsignaturaViewSet, NotaViewSet, prediccion_view
 
 router = DefaultRouter()
 router.register(r'asignaturas', AsignaturaViewSet, basename='asignatura')
 router.register(r'notas', NotaViewSet, basename='nota')
-
-@api_view(['GET'])
-def prediccion_view(request, asignatura_id):
-    asignatura = get_object_or_404(Asignatura, id=asignatura_id)
-    predicciones = asignatura.predecir_notas_faltantes()
-    return Response(predicciones)
 
 urlpatterns = [
     path('', include(router.urls)),
--- notas/views.py
+++ notas/views.py
@@ -1,11 +1,23 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 from .models import Asignatura, Nota
 from .serializers import AsignaturaSerializer, NotaSerializer
 
 class AsignaturaViewSet(viewsets.ModelViewSet):
     queryset = Asignatura.objects.all()
     serializer_class = AsignaturaSerializer
+    permission_classes = [IsAuthenticated]  # Solo usuarios autenticados pueden acceder
 
 class NotaViewSet(viewsets.ModelViewSet):
     queryset = Nota.objects.all()
     serializer_class = NotaSerializer
+    permission_classes = [IsAuthenticated]  # Solo usuarios autenticados pueden acceder
+
+@api_view(['GET'])
+def prediccion_view(request, asignatura_id):
+    asignatura = get_object_or_404(Asignatura, id=asignatura_id)
+    predicciones = asignatura.predecir_notas_faltantes()
+    return Response(predicciones)
