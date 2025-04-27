7f3f63ceef571d07b727302361007346ab450ad9
Tiffany Baron <tiffanybaron1998@gmail.com>
2025-04-27 18:13:17 -0400

    App Usuarios para que los estudiantes puedan registrarse y loguearse desde el Frontend

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'notas',
+    'usuarios', #Creacion de App Usuarios
 ]
 
 REST_FRAMEWORK = {
@@ -134,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuarios.Usuario' #Permite usar por defecto el modelo Usuario personalizado en la app usuarios
